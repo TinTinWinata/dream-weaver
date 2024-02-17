ccd1d10fe151be6c92b8ced6022875ddf7dc7245
root <root@DESKTOP-B8PLVL4.localdomain>
2024-02-18 00:08:11 +0700

    feat: frontend changed to typescript

---
+++ src/dream_weaver_frontend/src/App.tsx
@@ -0,0 +1,31 @@
+
+function App() {
+  const foo: string = 'Hello World'
+  return <p>{foo}</p>
+  // const [greeting, setGreeting] = useState('');
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   dream_weaver_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
+
+  // return (
+  //   <main>
+  //     <img src="/logo2.svg" alt="DFINITY logo" />
+  //     <br />
+  //     <br />
+  //     <form action="#" onSubmit={handleSubmit}>
+  //       <label htmlFor="name">Enter your name: &nbsp;</label>
+  //       <input id="name" alt="Name" type="text" />
+  //       <button type="submit">Click Me!</button>
+  //     </form>
+  //     <section id="greeting">{greeting}</section>
+  //   </main>
+  // );
+}
+
+export default App;
