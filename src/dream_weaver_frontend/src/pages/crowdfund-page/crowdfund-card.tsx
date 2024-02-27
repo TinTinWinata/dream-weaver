421cc4eadd3e54a2e86e5f6aa66706f9b385244f
tintinwinata <tintin6892@gmail.com>
2024-02-27 22:28:01 +0700

    fix: ui at crowdfunded page

--- src/dream_weaver_frontend/src/pages/crowdfund-page/crowdfund-card.tsx
+++ src/dream_weaver_frontend/src/pages/crowdfund-page/crowdfund-card.tsx
@@ -1,9 +1,9 @@
+import moment from "moment";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Paper from "../../components/paper";
 import ProgressBar from "../../components/progress-bar";
 import { TPost } from "../../types/post-type";
-import moment from "moment";
 
 // type TCrowdfundCardProps = {
 //   title: string;
@@ -24,7 +24,8 @@ export default function CrowdfundCard({ post }: { post: TPost }) {
       className="relative cursor-pointer"
     >
       <div className="flex gap-5">
-        <img className="rounded-md h-full w-[40%]" src={post.imageUrl} />
+        <div style={{backgroundImage: `url('${post.imageUrl}')`}} className="bg-cover w-[40%] h-[220px]  rounded-l-lg rounded-r-md"></div>
+        {/* <img className="rounded-md h-full w-[40%]" src={post.imageUrl} /> */}
         <div className="w-[60%] px-8 py-10 flex flex-col gap-5">
           <h1 className="text-xl font-bold">{post.description}</h1>
           <div className="flex flex-col gap-1">
